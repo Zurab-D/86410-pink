3207c27dc051f25bcd0a3e4b1fd4af237a0029be
Zurab-D <dzn@inbox.com>
2015-11-28 11:47:00 +0300

    добавил в form.html диалог success

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 (function () {
+  "use strict";
   var
     pricesSwitch1 = document.querySelector("#prices__table--switch-1"),
     pricesSwitch2 = document.querySelector("#prices__table--switch-2"),
@@ -7,14 +8,18 @@
     priceSwitch = document.querySelector("#price_switch"),
     priceSwitchLi = !!priceSwitch && priceSwitch.querySelectorAll("li"),
     nav = document.querySelector("#nav"),
-    menuBtn = document.querySelector("#menu_btn")
+    menuBtn = document.querySelector("#menu_btn"),
+    successDlg = document.querySelector("#success"),
+    successBtnClose = document.querySelector("#success_close"),
+    formBtnSubmit = document.querySelector("#submit_form"),
+    veil = document.querySelector("#veil")
   ;
 
   function removePriceTableSwitchClasses() {
     tablePrice.classList.remove("prices__table--switch-1");
     tablePrice.classList.remove("prices__table--switch-2");
     tablePrice.classList.remove("prices__table--switch-3");
-  };
+  }
 
   function removePriceSwitchClasses() {
     /*priceSwitchLi.forEach(function (item, i, priceSwitchLi){
@@ -24,7 +29,7 @@
     for (var i = 0; i < priceSwitchLi.length; i++) {
       priceSwitchLi[i].classList.remove("switch__item--active");
     }
-  };
+  }
 
   function switchItemsListener(self) {
     removePriceTableSwitchClasses();
@@ -33,21 +38,43 @@
     self.parentNode.classList.add("switch__item--active");
   }
 
-  !!pricesSwitch1 && pricesSwitch1.addEventListener('click', function(event){
+  function showVeil() {
+    !!veil && veil.classList.remove("veil--hidden");
+  }
+
+  function showSuccessDlg() {
+    showVeil();
+    !!successDlg && successDlg.classList.remove("success--hidden");
+  }
+
+  !!pricesSwitch1 && pricesSwitch1.addEventListener("click", function(event){
     switchItemsListener(this);
   });
 
-  !!pricesSwitch2 && pricesSwitch2.addEventListener('click', function(event){
+  !!pricesSwitch2 && pricesSwitch2.addEventListener("click", function(event){
     switchItemsListener(this);
   });
 
-  !!pricesSwitch3 && pricesSwitch3.addEventListener('click', function(event){
+  !!pricesSwitch3 && pricesSwitch3.addEventListener("click", function(event){
     switchItemsListener(this);
   });
 
-  menuBtn.addEventListener('click', function(event){
+  !!menuBtn && menuBtn.addEventListener("click", function(event){
     event.preventDefault();
     nav.classList.toggle("nav--expand");
     menuBtn.classList.toggle("menu-btn--close");
-  })
+  });
+
+  !!successBtnClose && successBtnClose.addEventListener("click", function(event){
+    event.preventDefault;
+    !!successDlg && successDlg.classList.add("success--hidden");
+    !!veil && veil.classList.add("veil--hidden");
+  });
+
+
+
+  !!formBtnSubmit && formBtnSubmit.addEventListener("click", function(event){
+    event.preventDefault();
+    showSuccessDlg();
+  });
 })();
