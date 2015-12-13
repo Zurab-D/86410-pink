75bd1846dff6ebbf50e31d3a56f985a36c25f2d0
Zurab-D <dzn@inbox.com>
2015-12-13 17:32:34 +0300

    Подкрутил JS - кнопки, слайдеры. Начал форму (Ajax)

--- js/script.js
+++ js/script.js
@@ -1,15 +1,53 @@
+/**
+ * Отправка формы по AJAX
+ */
+(function () {
+  var form = document.querySelector("#form");
+  if (!("FormData" in window) || (!form)) {
+    return;
+  }
+  var urlBase = "https://echo.htmlacademy.ru/adaptive&";
+
+  // запрашиваем по Ajax
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var data = new FormData(form);
+    request(data, urlBase, function (response) {
+      console.log("server response: \"" + response+"\"");
+    })
+  });
+
+  // функция запроса по Ajax
+  function request (dataQS, url, func) {
+    var xhr = new XMLHttpRequest();
+    var time = (new Date()).getTime();
+    var url = urlBase + time;
+    xhr.open("post", url);
+    xhr.addEventListener("readystatechange", function() {
+      if (xhr.readyState == 4) {
+        func(xhr.responseText);
+      };
+    });
+    xhr.send(dataQS);
+  };
+})();
+
+
+
+/**
+ * Обработка кнопок, переключателей и пр.
+ */
 (function () {
   "use strict";
   var
+    DIRECTION_LEFT = 0,
+    DIRECTION_RIGHT = 1,
     priceSwitch = document.querySelector(".switch--prices"),
-    priceSwitchItems = !!priceSwitch && priceSwitch.querySelectorAll(".switch__item"),
     priceSwitchLabels = !!priceSwitch && priceSwitch.querySelectorAll(".switch__label"),
-    tablePrice = document.querySelector(".prices__table"),
-    nav = document.querySelector(".nav"),
     menuBtn = document.querySelector("#menu_btn"),
     successDlg = document.querySelector(".success"),
     successBtnClose = document.querySelector(".success__btn"),
-    formBtnSubmit = document.querySelector(".submit__btn"),
+    //formBtnSubmit = document.querySelector(".submit__btn"),
     daysCount = document.querySelector("#days_count"),
     daysCountMinus = document.querySelector("#days_count_minus"),
     daysCountPlus = document.querySelector("#days_count_plus"),
@@ -19,25 +57,6 @@
     sliderReviewArrowLeft = document.querySelector(".slider-review__arrow--left"),
     sliderReviewArrowRight = document.querySelector(".slider-review__arrow--right")
   ;
-
-
-
-  // удаляем пометки (классы) активности у таблицы и у переключателей слайдера цен
-  function removePriceSwitchClasses() {
-    tablePrice.className = "prices__table";
-    for (var i = 0; i < priceSwitchItems.length; i++) {
-      priceSwitchItems[i].classList.remove("switch__item--active");
-    }
-  }
-
-
-
-  // обработчик переключателей слайдера
-  function switchItemsListener(self) {
-    removePriceSwitchClasses();
-    !!tablePrice && tablePrice.classList.add(self.id);
-    self.parentNode.classList.add("switch__item--active");
-  }
 
 
 
@@ -52,26 +71,31 @@
 
 
 
-  // показать диалог успеха
-  function showSuccessDlg(event) {
-    event.preventDefault();
-    showOverlay();
-    !!successDlg && successDlg.classList.remove("success--hidden");
+  // обработчик переключателей слайдера
+  function switchItemsListener(self) {
+    var tablePrice = document.querySelector(".prices__table");
+    if (!!tablePrice) {
+      removePriceSwitchClasses();
+      tablePrice.classList.add(self.id);
+      self.parentNode.classList.add("switch__item--active");
+
+      // удаляем пометки (классы) активности у таблицы и у переключателей слайдера цен
+      function removePriceSwitchClasses() {
+        var priceSwitchItems = !!priceSwitch && priceSwitch.querySelectorAll(".switch__item");
+        tablePrice.className = "prices__table";
+        for (var i = 0; i < priceSwitchItems.length; i++) {
+          priceSwitchItems[i].classList.remove("switch__item--active");
+        }
+      }
+    }
   }
-  // скрыть диалог успеха
-  function hideSuccessDlg(event) {
-    event.preventDefault;
-    !!successDlg && successDlg.classList.add("success--hidden");
-    hideOverlay();
-  };
 
 
 
   // назначаем обработчики переключателям слайдера цен
   if (!!priceSwitchLabels) {
     for (var i = 0; i < priceSwitchLabels.length; i++) {
-      //!!priceSwitchLabels[i] &&
-        priceSwitchLabels[i].addEventListener("click", function(event) {
+      priceSwitchLabels[i].addEventListener("click", function(event) {
         event.preventDefault();
         switchItemsListener(this);
       })
@@ -81,6 +105,7 @@
 
   // спрятать / показать мобильное меню
   !!menuBtn && menuBtn.addEventListener("click", function(event){
+    var nav = document.querySelector(".nav");
     event.preventDefault();
     nav.classList.toggle("nav--expand");
     menuBtn.classList.toggle("menu-btn--close");
@@ -90,29 +115,31 @@
 
 
   // показать диалог успеха
-  !!formBtnSubmit && formBtnSubmit.addEventListener("click", function(event) {
-    showSuccessDlg(event);
+  /*!!formBtnSubmit && formBtnSubmit.addEventListener("click", function(event) {
+    event.preventDefault();
+    showOverlay();
+    !!successDlg && successDlg.classList.remove("success--hidden");
   });
   // закрыть диалог успеха
   !!successBtnClose && successBtnClose.addEventListener("click", function(event) {
-    hideSuccessDlg(event);
-  });
+    event.preventDefault();
+    !!successDlg && successDlg.classList.add("success--hidden");
+    hideOverlay();
+  });*/
 
 
 
   // увеличить значение поля input.value
-  function inputValueInc(input, event) {
+  function inputValueInc(input) {
     if (!!input) {
-      event.preventDefault();
       var sum = parseInt(input.value);
       !!sum || sum === 0 ? sum = sum + 1 : null;
       !!sum ? input.value = sum : null;
     }
   };
   // уменьшить значение поля input.value
-  function inputValueDec(input, event) {
+  function inputValueDec(input) {
     if (!!input) {
-      event.preventDefault();
       var sum = parseInt(input.value);
       !!sum ? sum = sum - 1 : null;
       !!sum || sum === 0 ? input.value = sum : null;
@@ -123,32 +150,32 @@
 
   // количество дней - минус
   !!daysCountMinus && daysCountMinus.addEventListener("click", function(event) {
-    inputValueDec(daysCount, event);
+    event.preventDefault();
+    inputValueDec(daysCount);
   });
   // количество дней - плюс
   !!daysCountPlus && daysCountPlus.addEventListener("click", function(event) {
-    inputValueInc(daysCount, event);
+    event.preventDefault();
+    inputValueInc(daysCount);
   });
 
 
 
   // количество попутчиков - минус
   !!personsCountMinus && personsCountMinus.addEventListener("click", function(event) {
-    inputValueDec(personsCount, event);
+    event.preventDefault();
+    inputValueDec(personsCount);
   });
   // количество попутчиков - плюс
   !!personsCountPlus && personsCountPlus.addEventListener("click", function(event) {
     event.preventDefault();
-    inputValueInc(personsCount, event);
+    inputValueInc(personsCount);
   });
 
 
 
   // индекс предыдущей страницы слайдера
   function getSliderPrevIndex (currentIndex, sliderItemCount) {
-    /*  ВОПРОС!
-      Нужно ли обязательно проверять: переданы ли параметры и тип параметров?
-    */
     currentIndex = currentIndex - 1;
     if (currentIndex < 0) {currentIndex = sliderItemCount - 1}
     return currentIndex;
@@ -160,41 +187,36 @@
 
 
 
-
-  // процедура сдвига слайдера на radio - влево
-  function sliderRadioGoLeft (switchClassSelector) {
+  // процедура сдвига слайдера на input[type=radio] - вправо
+  // direction: DIRECTION_LEFT, DIRECTION_RIGHT; default value: DIRECTION_LEFT
+  function switchSlider (switchClassSelector, direction) {
     var switches = document.querySelectorAll(switchClassSelector);
+    if (direction === undefined) {
+      direction = DIRECTION_LEFT;
+    };
     for (var i = 0; i < switches.length; i++) {
       if (switches[i].checked) {
-        var prevIndex = getSliderPrevIndex(i, 3);
-        switches[prevIndex].checked = 1;
-        return prevIndex;
+        var newIndex;
+        direction === DIRECTION_LEFT ?
+          newIndex = getSliderPrevIndex(i, 3) :
+          newIndex = getSliderNextIndex(i, 3)
+        ;
+        switches[newIndex].checked = 1;
+        return newIndex;
       }
     }
   };
-  // процедура сдвига слайдера на radio - вправо
-  function sliderRadioGoRight (switchClassSelector) {
-    var switches = document.querySelectorAll(switchClassSelector);
-    for (var i = 0; i < switches.length; i++) {
-      if (switches[i].checked) {
-        var nextIndex = getSliderNextIndex(i, 3);
-        switches[nextIndex].checked = 1;
-        return nextIndex;
-      }
-    }
-  };
-
 
 
 
   // слайдер slider-review - влево
   !!sliderReviewArrowLeft && sliderReviewArrowLeft.addEventListener("click", function(event) {
     event.preventDefault();
-    sliderRadioGoLeft(".switch__radio");
+    switchSlider(".switch__radio", DIRECTION_LEFT);
   });
   // слайдер slider-review - вправо
   !!sliderReviewArrowRight && sliderReviewArrowRight.addEventListener("click", function(event) {
     event.preventDefault();
-    sliderRadioGoRight(".switch__radio");
+    switchSlider(".switch__radio", DIRECTION_RIGHT);
   });
 }());
